f82d7d38304b5cba578ea3a6cb867f970cba4b95
tomalak7 <sashatomalak@icloud.com>
2025-04-11 17:12:30 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,8 @@
 var addressPoints = [
   [
     "Effective field theory for radiative corrections to charged-current processes: neutron decay and Vud<br />Theory Workshop on Neutrino Physics, Center for Theoretical Underground Physics and Related Areas, Lead, SD, USA",
-    44.3471,
-    -103.7763
+    -44.3471,
+    103.7763
   ],
   [
     "On the uncertainty of CEvNS cross sections (online)<br />Magnificent CEvNS 2025, Brazilian Center for Physics Research, Rio de Janeiro, Brazil",
@@ -11,7 +11,7 @@ var addressPoints = [
   ],
   [
     "QED Nuclear Medium Effects at EIC Energies<br />Bridging Theory and Experiment at the Electron-Ion Collider, Institute for Nuclear Theory, Seattle, WA, USA",
-    47.6556,
-    -122.3035
+    -47.6556,
+    122.3035
   ]
 ];
