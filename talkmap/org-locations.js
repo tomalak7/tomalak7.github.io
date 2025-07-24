2da6809fcbaedd71508de38ec46c7bb86f1e4ed7
tomalak7 <sashatomalak@icloud.com>
2025-07-24 11:52:27 +0800

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,11 +1,16 @@
 var addressPoints = [
   [
+    "Effective field theory for radiative corrections to charged-current processes and hadronic uncertainty in neutral-current processes<br />Baryons 2025, International Convention Center, Jeju, South Korea",
+    33.2383,
+    126.4212
+  ],
+  [
     "Constraints on new physics with (anti)neutrino–nucleon scattering data<br />15th International Workshop on Neutrino-Nucleus Interactions, Mainz, Germany",
     49.9906,
     8.2345
   ],
   [
-    "Effective field theory for radiative corrections to charged-current processes: neutron decay and Vud<br />Theory Workshop on Neutrino Physics, Center for Theoretical Underground Physics and Related Areas, Lead, SD, USA",
+    "Effective field theory for radiative corrections to charged-current processes and hadronic uncertainty in neutral-current processes<br />Theory Workshop on Neutrino Physics, Center for Theoretical Underground Physics and Related Areas, Lead, SD, USA",
     44.3471,
     -103.7763
   ],
