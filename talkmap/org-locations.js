7d01537df8e8e6b198ec0367ceb22fe4b8986204
tomalak7 <sashatomalak@icloud.com>
2025-09-12 11:00:13 +0800

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -20,23 +20,8 @@ var addressPoints = [
     8.2345
   ],
   [
-    "Effective field theory for radiative corrections to charged-current processes and hadronic uncertainty in neutral-current processes<br />Theory Workshop on Neutrino Physics, Center for Theoretical Underground Physics and Related Areas, Lead, SD, USA",
-    44.3471,
-    -103.7763
-  ],
-  [
-    "On the uncertainty of CEvNS cross sections (online)<br />Magnificent CEvNS 2025, Brazilian Center for Physics Research, Rio de Janeiro, Brazil",
-    -22.9524,
-    -43.1709
-  ],
-  [
     "QED Nuclear Medium Effects at EIC Energies<br />The 26th international symposium on spin physics (SPIN2025), Qingdao (Tsingtao), Shandong Province, China",
     36.0584,
     120.3254
-  ],
-  [
-    "Research Visit from 18-06-2025 to 28-06-2025, JGU Mainz, Mainz, Germany",
-    49.9906,
-    8.2345
   ]
 ];
