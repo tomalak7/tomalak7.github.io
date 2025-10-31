4d5afb1132c33cc45719023df604097f60c98de8
tomalak7 <sashatomalak@icloud.com>
2025-10-31 17:44:34 +0800

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,28 +5,8 @@ var addressPoints = [
     126.4212
   ],
   [
-    "Radiative corrections to neutrino-nucleon/nucleus cross section <br />Neutrino Interactions for Current and Future Experiments, KEK, Tsukuba, Japan",
-    36.1496,
-    140.0758
-  ],
-  [
-    "Radiative corrections and cross sections in neutrino physics, <br />vSTEP 2025, Beijing, China",
-    40.1304,
-    116.4921
-  ],
-  [
     "Charged-current neutrino scattering cross sections and radiative corrections, <br />NuPhyR, Zhuhai, Guangdong, China",
     22.2000,
     113.5000
-  ],
-  [
-    "Constraints on new physics with (anti)neutrino–nucleon scattering data<br />15th International Workshop on Neutrino-Nucleus Interactions, Mainz, Germany",
-    49.9906,
-    8.2345
-  ],
-  [
-    "QED Nuclear Medium Effects at EIC Energies<br />The 26th international symposium on spin physics (SPIN2025), Qingdao (Tsingtao), Shandong Province, China",
-    36.0584,
-    120.3254
   ]
 ];
