1081e5d0de24e8ff55290842ec1a612a68b31fad
tomalak7 <sashatomalak@icloud.com>
2026-01-18 12:04:59 +0800

    Add new locations to org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -8,5 +8,20 @@ var addressPoints = [
     "Charged-current neutrino scattering cross sections and radiative corrections, <br />NuPhyR, Zhuhai, Guangdong, China",
     22.2000,
     113.5000
+  ],
+  [
+    "Low-energy electroweak physics, <br />Sun Yat-sen University, Guangzhou, Guangdong, China",
+    23.0966,
+    113.3000
+  ],
+  [
+    "Neutrino physics, <br />X Winter School for High-School Students in Physics, Kyiv, Ukraine",
+    50.4428,
+    30.5094
+  ],
+  [
+    "Radiative corrections in electron and neutrino scattering, <br />Shandong University, Qingdao, Shandong, China",
+    36.3800,
+    120.6591
   ]
 ];
